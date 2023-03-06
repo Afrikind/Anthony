462af75415ee54bd86d164c1aea320d2bb50f7c7
Afrikind <anthony.madukaku@acity.edu.gh>
2023-03-06 11:07:43 +0000

    fed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Header toggle 
 
-let MenuBtn = document.getElementById('MenuBtn')
+// let MenuBtn = document.getElementById('MenuBtn')
 
-MenuBtn.addEventListener('click', function(e){
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    this.classList.toggle('fa-xmark')
-});
+// MenuBtn.addEventListener('click', function(e){
+//     document.querySelector('body').classList.toggle('mobile-nav-active');
+//     this.classList.toggle('fa-xmark')
+// });
 
 
 //Typing Effect
